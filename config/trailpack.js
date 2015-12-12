0c4eaffeb6c24d105ce49cb98f1c50b9ac3c7fd5
Travis Webb <me@traviswebb.com>
2015-12-11 22:54:00 -0500

    [config] disabled hapi trailpack until trailpack-hapi#3 is resolved

--- config/trailpack.js
+++ config/trailpack.js
@@ -16,7 +16,7 @@ module.exports = {
     require('trailpack-core'),
     require('trailpack-repl'),
     require('trailpack-router'),
-    require('trailpack-hapi')
+    //require('trailpack-hapi')
   ],
 
   paths: {
